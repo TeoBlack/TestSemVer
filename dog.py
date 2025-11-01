8e9f8bb8c4a6ca5f36e889f7153870377fa500b3
TeoBlack <49093088+TeoBlack@users.noreply.github.com>
2025-11-01 09:49:11 +0300

    Add files via upload
    Adding file with class and using class method in main

---
+++ dog.py
@@ -0,0 +1,9 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        print("Гав")
+
+    def get_name(self):
+        return self.name
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from dog import Dog
 print("hello world\n")
+
+def main():
+    my_dog = Dog("Бобик")
+    my_dog.bark()
+
+if __name__ == "__main__":
+    main()
